e5277a85471ac9ddee4da1b96e02891d92390d46
mintchococookie <parksungju90@gmail.com>
2020-07-23 17:57:48 -0500

    0206

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        int studentAge = 15;
-        double studentGPA = 3.45;
+//        int studentAge = 15;
+        double studentGPA = 3.78;
         String studentFirstName = "Sungju";
         String studentLastName = "Park";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
-        boolean hasPerfectAttendance = true;
+//        char studentFirstInitial = studentFirstName.charAt(0);
+//        char studentLastInitial = studentLastName.charAt(0);
+//        boolean hasPerfectAttendance = true;
 
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
-        System.out.println(studentFirstName);
-        System.out.println(studentLastName);
-
+//        System.out.println(studentAge);
+//        System.out.println(studentGPA);
+//        System.out.println(studentFirstInitial);
+//        System.out.println(studentLastInitial);
+//        System.out.println(hasPerfectAttendance);
+//        System.out.println(studentFirstName);
+//        System.out.println(studentLastName);
+        System.out.println(studentFirstName + " " + studentLastName +
+                " has a GPA of " + studentGPA);
     }
 }
