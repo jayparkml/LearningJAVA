466d7f4ba9d1bb6ecce5612c2c6029f9e36cf7af
mintchococookie <parksungju90@gmail.com>
2020-07-23 17:53:52 -0500

    2

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,20 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
+        String studentFirstName = "Sungju";
+        String studentLastName = "Park";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
         boolean hasPerfectAttendance = true;
+
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
 
     }
 }
