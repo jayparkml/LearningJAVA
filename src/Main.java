8b4cefd66b5a2a4bf387b693b4ef2f26891837ad
mintchococookie <parksungju90@gmail.com>
2020-07-24 17:29:28 -0500

    Ch6 done

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,7 @@
-//public class Main {
-//
-//
-//    public static void main(String[] args) {
-//
-//        Student studentA = new Student("Jay","Park",2020,3.8,"M.E");
-//        Student studentB = new Student("Kathryn","Hodge",2021,3.7,"C.S");
-//
-//        studentA.incrementExpectedGraduationYear();
-//        System.out.println(studentA.expYearGraduate);
-//
-//
-//
-//
-//
-//
-//////////////////////////////////////////////////////////////////////////////////
-////        Triangle triangleA = new Triangle(15,8,15,8,17);
-////        Triangle triangleB = new Triangle(3,2.59,3,3,3);
-////
-////        //instance method -> need to save data to use function built in class
-////        //class method -> can directly put data into function like Math.pow(a,b)
-////
-////        double triangleAArea = triangleA.findArea();
-////        System.out.println(triangleAArea);
-////
-////        double triangleBArea = triangleB.findArea();
-////        System.out.println(triangleBArea);
-////
-////        System.out.println(triangleA.sideLenThree);
-////        System.out.println(triangleB.base);
-////
-////        System.out.println(Triangle.numOfSides);
-////
-//
-//    }
-//}
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+}
