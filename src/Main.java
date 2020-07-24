0065b4dab97e27f43dcbac228f582758775d40af
mintchococookie <parksungju90@gmail.com>
2020-07-24 14:44:28 -0500

    teatime function

--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,27 @@
-import java.util.Scanner; // Import to use the scanner
+import java.util.Scanner;
 
 public class Main {
 
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time.....");
+        System.out.println("Type in a random word and press Enter to start developer team time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time!");
+    }
+
     public static void main(String[] args) {
+        System.out.println("Welcome to your new job");
 
-            System.out.println("Let's calculate the area of a triangle");
+        announceDeveloperTeaTime();
 
-            Scanner input = new Scanner(System.in);
+        System.out.println("Write Code");
+        System.out.println("Review Code");
 
-            System.out.println("Please input the base of the triangle (in inches).");
-            double base = input.nextDouble();
+        announceDeveloperTeaTime();
 
-            while (base <= 0) {
-                System.out.println("That's invalid. Please input the base of the triangle (in inches).");
-                base = input.nextDouble();
-            }
+        System.out.println("Get promoted!");
 
-            System.out.println("Please input the height of the triangle (in inches).");
-            double height = input.nextDouble();
-            while (height <= 0) {
-                System.out.println("That's invalid. Please input the height of the triangle (in inches).");
-                height = input.nextDouble();
-            }
-
-            double area = (base * height) / 2;
-            System.out.println("The area is " + area);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        ///////////////////////////////////////////////////////////
-//        String question = "What is 1 * 2? ";
-//        String choiceOne = "one";
-//        String choiceTwo = "two";
-//        String choiceThree = "three";
-//        boolean getAnswerWrong = true;
-//
-//        String correctAnswer = choiceTwo;
-//
-//        System.out.println(question);
-//
-//        Scanner input = new Scanner(System.in);
-//
-//
-//
-//        while (getAnswerWrong) {
-//            String uinput = input.next();
-//            if (uinput.toLowerCase().equals(correctAnswer)) { //.equals = case sensitive so make all lowercase
-//                System.out.println("Congratulation!!");
-//                getAnswerWrong = false;
-//            } else {
-//                System.out.println("Try again");
-//            }
-//        }
-
-
-        ////////////////////////////////////////////////////
-//        Scanner input = new Scanner(System.in);
-//        boolean isOnRepeat = true;
-//
-//        while (isOnRepeat) {
-//            System.out.println("Playing current song");
-//            System.out.println("Would you like to take this song off of repeat? If so, answer yes");
-//            String userInput = input.next();
-//            if (userInput.equals("yes")) { //equals = string operation
-//                isOnRepeat = false;
-//            }
-//
-//        }
-//        System.out.println("Playing next song");
-
-
-
-
-
-
-
-        ///////////////////////////////////////////////////////////////////////////
-//        System.out.println("Pick a number between 1 and 10");
-//        Scanner input = new Scanner(System.in);
-//
-//        int inputtedNum = input.nextInt();
-//
-//        if (inputtedNum < 5) {
-//            System.out.println("Enjoy the good luck a friend brings you");
-//            //print out "Enjoy the good luck a friend brings you"
-//        } else {
-//            //print out ""
-//            System.out.println("Your shoe selection will make you very happy today");
-//        }
     }
 }
+
