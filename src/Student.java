7d4ff19b3db5f82c7553a6444005b13d436b06a8
mintchococookie <parksungju90@gmail.com>
2020-07-24 17:28:41 -0500

    Chapter6 done

---
+++ src/Student.java
@@ -0,0 +1,25 @@
+//public class Student {
+//
+//    String firstName;
+//    String lastName;
+//    double expYearGraduate;
+//    double gpa;
+//    String declaredMajor;
+//
+//    public Student (String firstName,
+//                    String lastName,
+//                    double expYearGraduate,
+//                    double gpa,
+//                    String declaredMajor) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.expYearGraduate = expYearGraduate;
+//        this.gpa = gpa;
+//        this.declaredMajor = declaredMajor;
+//    }
+//
+//    public void incrementExpectedGraduationYear() {
+//        this.expYearGraduate = this.expYearGraduate + 1;
+//    }
+//
+//}
