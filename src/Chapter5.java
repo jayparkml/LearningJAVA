6273180a08fe2a97c0c9d527c4d8c2b9f175b749
mintchococookie <parksungju90@gmail.com>
2020-07-24 15:52:33 -0500

    chapter5

---
+++ src/Chapter5.java
@@ -0,0 +1,76 @@
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static double calculateEmployeesSalary(double hours,
+//                                                  double amount,
+//                                                  double vacation) {
+//        if (hours < 0) {
+//            return -1;
+//        }
+//
+//        if (amount < 0) {
+//            return -1;
+//        }
+//
+//        if (vacation < 0) {
+//            return -1;
+//        }
+//
+//        double Salary = hours * 48 * amount + vacation * 8 * amount;//48 weeks/yr
+//        return Salary;
+//    }
+//
+//
+/////////////////////////////////////////////////////////////////////////////
+////    public static double calculateTotalMealPrice(double listedMealPrice,
+////                                               double tipRate,
+////                                               double taxRate) {
+////        double tip = tipRate * listedMealPrice;
+////        double tax = taxRate * listedMealPrice;
+////        double result = listedMealPrice + tip + tax;
+////        return result;
+////    }
+//
+//
+/////////////////////////////////////////////////////////////////////////////////
+////    public static void announceDeveloperTeaTime() {
+////        System.out.println("Waiting for developer tea time.....");
+////        System.out.println("Type in a random word and press Enter to start developer team time");
+////        Scanner input = new Scanner(System.in);
+////        input.next();
+////        System.out.println("It's developer tea time!");
+////    }
+//
+//    public static void main(String[] args) {
+//        double mySalary = calculateEmployeesSalary(40,40,10);
+//        System.out.println("My Salary is $"+mySalary);
+//
+//
+/////////////////////////////////////////////////////////////////////////
+////        double result = Math.pow(5, 4);
+////        System.out.println(result);
+//
+//
+//        ////////////////////////////////////////////////////////////
+////        double groupTotalMealPrice = calculateTotalMealPrice(100,.2,.08);
+////        System.out.println(groupTotalMealPrice);
+////
+////        double individualMealPrice = groupTotalMealPrice / 5;
+////        System.out.println(individualMealPrice);
+//
+//
+////        System.out.println("Welcome to your new job");
+////
+////        announceDeveloperTeaTime();
+////
+////        System.out.println("Write Code");
+////        System.out.println("Review Code");
+////
+////        announceDeveloperTeaTime();
+////
+////        System.out.println("Get promoted!");
+//
+//
+//    }
+//}
+//
